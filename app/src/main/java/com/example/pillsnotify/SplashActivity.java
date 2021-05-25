67b0bbb803fb6f7d4e3a285d5a1a91b8b7e43f3a
AndrewAntepa <55337950+AndrewAntepa@users.noreply.github.com>
2021-05-25 14:39:29 +0800

    new

---
+++ app/src/main/java/com/example/pillsnotify/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.pillsnotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageSplash);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                SplashActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageView.setAnimation(animation);
+    }
+}
